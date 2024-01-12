7acb5830bb8126de5d0546c19c516cf2f342c2c0
Phil D <pcdrummond@icloud.com>
2024-01-11 18:10:37 -0800

    Sets up Chakra-UI.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import '../styles/globals.css';
+import customTheme from '../theme'; // Import your custom theme
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Header/>
+    <ChakraProvider theme={customTheme}> {/* Use your custom theme */}
+      <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </ChakraProvider>
   );
 }
---
+++ theme/index.js
@@ -0,0 +1,8 @@
+// theme/index.js
+import { extendTheme } from '@chakra-ui/react';
+
+const customTheme = extendTheme({
+  // Your custom theme config goes here
+});
+
+export default customTheme;
